9cd3f4c75c4eb9930011b17012341f8f9430e60b
Andy Flack <flack.ap@gmail.com>
2014-11-12 19:42:55 -0500

    added news feed

---
+++ src/scripts/main.js
@@ -0,0 +1,23 @@
+function newsFeed() {
+   $.ajax({
+     url: document.location.protocol + '//ajax.googleapis.com/ajax/services/feed/load?v=1.0&num=10&callback=?&q=' + encodeURIComponent("http://apps.shareholder.com/rss/rss.aspx?channels=7247&companyid=ABEA-4SW9EX&sh_auth=3611633203%2E0%2E0%2E41957%2Ebbbf2f040bc3a66f02b1974e7503788e"),
+     dataType: 'json',
+     success: function(data) {
+       
+       var stories = data.responseData.feed.entries;
+       var fragment = '';
+       
+       if(stories) {
+          
+          for(var i=0;i < 2; i++) {
+            if (stories[i]) {
+              fragment += '<a class="news-link" href="'+stories[i].link+'">'+stories[i].title+'</a><br><p class="news-date"></p>';
+            }
+          }
+         
+         $(".news-feed").append(fragment);
+         }
+       }
+   });
+}
+
