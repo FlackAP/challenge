d962ec609a044fcf9270029c11c81b6e2cff326c
FlackAP <flack.ap@gmail.com>
2014-11-21 03:14:18 -0500

    finished up footer and fixed up that awful jamboscript

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -4,19 +4,25 @@ function newsFeed() {
      dataType: 'json',
      success: function(data) {
        
+       // let's set stories to equal our JSON from the rss feed (thanks google!)
        var stories = data.responseData.feed.entries;
-       var fragment = '';
+       var html = '';
        
        if(stories) {
-          
           for(var i=0;i < 2; i++) {
             if (stories[i]) {
-              // I made the decision to hard-code author/date here as I couldn't find the info in the RSS feed 
-              fragment += '<p class="news-link" style="margin-bottom:0; padding-bottom:18px;"><a href="'+stories[i].link+'"><strong>'+stories[i].title+'</strong></a><br><span>By Caroline Potter on October 1, 2014</span></p>';
+              // take the published date and store it in a variable called date, then trim it down using substring
+              var date = stories[i].publishedDate;
+              var pubDate = date.substring(0, date.length - 14)
+              //let's check to see if we trimmed off the right amount of characters from the date string
+              console.log(pubDate)
+              // I made the decision to omit the author as the information was not provided in the RSS feed
+              html += '<p class="news-link" style="margin-bottom:0; padding-bottom:18px;"><a href="'+stories[i].link+'"><strong>'+stories[i].title+'</strong></a><br><span>'+pubDate+'</span></p>';
             }
           }
          
-         $(".news-feed").append(fragment);
+         // append our html fragment to newsfeed
+         $(".news-feed").append(html);
          }
        }
    });
